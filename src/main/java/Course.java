c4c506304bd08303e6ee01dae5ceb89aaf9eb1ee
armin afsharian <armin.afshariian@gmail.com>
2021-02-27 13:10:01 +0330

    defining fields for each class

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,3 +1,10 @@
 public class Course {
-
+    private String code;
+    private String name;
+    private String instructor;
+    private int units;
+    private String classTime;
+    private String examTime;
+    private int capacity;
+    private String[] prerequisites;
 }
--- src/main/java/System.java
+++ src/main/java/System.java
@@ -1,3 +1,6 @@
-public class System {
+import java.util.ArrayList;
 
+public class System {
+    private ArrayList<Student> students = new ArrayList<Student>();
+    private ArrayList<Course> courses = new ArrayList<Course>();
 }
