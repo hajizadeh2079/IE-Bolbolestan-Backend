b4c8d3abcef7ab71d60b13b5d9b26c1dfaadff7a
armin afsharian <armin.afshariian@gmail.com>
2021-02-27 16:31:51 +0330

    impelmenting addStudent function

--- src/main/java/System.java
+++ src/main/java/System.java
@@ -1,6 +1,14 @@
 import java.util.ArrayList;
+import org.json.simple.JSONObject;
 
 public class System {
     private ArrayList<Student> students = new ArrayList<Student>();
     private ArrayList<Course> courses = new ArrayList<Course>();
+    public void addStudent(JSONObject jo){
+        String id = (String)jo.get("studentId");
+        String name = (String)jo.get("name");
+        String enteredAt = (String)jo.get("enteredAt");
+        Student student = new Student(id, name, enteredAt);
+        students.add(student);
+    }
 }
