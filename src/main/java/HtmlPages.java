3e4acc55dd32e2c9e6c9b3fa2989b5c70332a025
armin afsharian <armin.afshariian@gmail.com>
2021-03-09 12:17:27 +0330

    Complete plan html page

--- src/main/java/HtmlPages.java
+++ src/main/java/HtmlPages.java
@@ -114,12 +114,27 @@ public class HtmlPages {
     }
 
     public String planPage(Object data) {
+        Student student = (Student) data;
         String html = "<html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Plan</title>" +
               "<style> table{width: 100%; text-align: center;} table, th, td{border: 1px solid black;border-collapse: collapse;" +
                "</style></head><body><table><tr><th></th><th>07:30-09:00</th><th>09:00-10:30</th><th>10:30-12:00</th>" +
-                "<th>14:00-15:30</th><th>16:00-17:30</th></tr><tr><td>Saturday</td><td></td><td></td><td></td><td></td><td></td></tr>" +
-                "<tr><td>Sunday</td><td></td><td></td><td></td><td></td><td></td></tr><tr><td>Monday</td><td></td><td></td><td></td><td></td><td></td></tr>" +
-                "<tr><td>Tuesday</td><td></td><td></td><td></td><td></td><td></td></tr><tr><td>Wednesday</td><td></td><td></td><td></td><td></td><td></td></tr>";
+                "<th>14:00-15:30</th><th>16:00-17:30</th></tr>" +
+                "<tr id=\"Saturday\"><td>Saturday</td><td id=\"07:30\"></td><td id=\"09:00\"></td><td id=\"10:30\"></td><td id=\"14:00\"></td><td id=\"16:00\"></td></tr>" +
+                "<tr id=\"Sunday\"><td>Sunday</td><td id=\"07:30\"></td><td id=\"09:00\"></td><td id=\"10:30\"></td><td id=\"14:00\"></td><td id=\"16:00\"></td></tr>" +
+                "<tr id=\"Monday\"><td>Monday</td><td id=\"07:30\"></td><td id=\"09:00\"></td><td id=\"10:30\"></td><td id=\"14:00\"></td><td id=\"16:00\"></td></tr>" +
+                "<tr id=\"Tuesday\"><td>Tuesday</td><td id=\"07:30\"></td><td id=\"09:00\"></td><td id=\"10:30\"></td><td id=\"14:00\"></td><td id=\"16:00\"></td></tr>" +
+                "<tr id=\"Wednesday\"><td>Wednesday</td><td id=\"07:30\"></td><td id=\"09:00\"></td><td id=\"10:30\"></td><td id=\"14:00\"></td><td id=\"16:00\"></td></tr>" +
+                "</table></body></html>";
+
+        Document doc = Jsoup.parse(html);
+        for(Course course: student.getWeeklySchedule().getCourses()) {
+            for (String day: course.getClassTimeDays()) {
+                Element tableRow = doc.getElementById(day);
+                Element td = tableRow.getElementById(course.getClassTimeStart().toString());
+                td.append(course.getName());
+            }
+        }
+        html = doc.toString();
         return html;
     }
 
