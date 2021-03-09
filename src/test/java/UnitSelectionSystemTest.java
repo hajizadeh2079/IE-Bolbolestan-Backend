8230691a5aa5b01eaeee53384b900238ea733065
armin afsharian <armin.afshariian@gmail.com>
2021-03-09 16:04:02 +0330

    Add addToWeeklyScheduleTest function

--- src/test/java/UnitSelectionSystemTest.java
+++ src/test/java/UnitSelectionSystemTest.java
@@ -2,6 +2,8 @@ import org.json.simple.JSONArray;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -25,7 +27,7 @@ public class UnitSelectionSystemTest {
     }
 
     @Test
-    public void findCourseTest() throws Exception{
+    public void findCourseTest() throws Exception {
         Course course = unitSelectionSystem.findCourse("8101005", "01");
         String expected = "Calculus 2";
         String actual = course.getName();
@@ -36,13 +38,37 @@ public class UnitSelectionSystemTest {
     }
 
     @Test
-    public void findStudentTest() throws Exception{
+    public void findStudentTest() throws Exception {
         Student student = unitSelectionSystem.findStudent("810197227");
         String expected = "Karimi";
         String actual = student.getSecondName();
         assertEquals(expected, actual);
         assertThrows(StudentNotFound.class, () -> {
             unitSelectionSystem.findStudent("810197452");
+        });
+    }
+
+    @Test
+    public void addToWeeklyScheduleTest() throws Exception {
+        unitSelectionSystem.addToWeeklySchedule("810196285", "8101010", "01");
+        Student student = unitSelectionSystem.findStudent("810196285");
+        WeeklySchedule weeklySchedule = student.getWeeklySchedule();
+        ArrayList<Course> courses = weeklySchedule.getCourses();
+        String expected = "Discrete Mathematics";
+        String actual = weeklySchedule.getCourses().get(0).getName();
+        assertEquals(expected, actual);
+        assertThrows(ClassTimeCollisionError.class, () -> {
+            unitSelectionSystem.addToWeeklySchedule("810196285", "8101021", "01");
+        });
+        unitSelectionSystem.addToWeeklySchedule("810195115", "8101010", "01");
+        assertThrows(ExamTimeCollisionError.class, () -> {
+            unitSelectionSystem.addToWeeklySchedule("810195115", "8101011", "01");
+        });
+        assertThrows(PrerequisitesError.class, () -> {
+            unitSelectionSystem.addToWeeklySchedule("810196285", "8101011", "01");
+        });
+        assertThrows(PrerequisitesError.class, () -> {
+            unitSelectionSystem.addToWeeklySchedule("810196285", "8101005", "01");
         });
     }
 }
