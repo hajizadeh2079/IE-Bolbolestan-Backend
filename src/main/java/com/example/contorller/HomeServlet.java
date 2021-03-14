6ba09f820b1a8130fd5abeb63060e27af0941f81
armin afsharian <armin.afshariian@gmail.com>
2021-03-14 18:27:40 +0330

    Fix a minor bug in login.jsp and update doGet method in Home

--- src/main/java/com/example/contorller/HomeServlet.java
+++ src/main/java/com/example/contorller/HomeServlet.java
@@ -1,12 +1,17 @@
 package com.example.contorller;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "home", value = "/")
+@WebServlet(name = "home", value = "")
 public class HomeServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Object stdId = request.getAttribute("std_id");
+        if (stdId == null)
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
