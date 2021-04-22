4b4d9cbb8bcb862fc0ac612ba4ebfe6e0db94a0b
armin__af <armin.afshariian@gmail.com>
2021-04-22 18:01:49 +0430

    Complete API for get profile via id

--- src/main/java/IE/server/controllers/HomeController.java
+++ src/main/java/IE/server/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package IE.server.controllers;
 
+import IE.server.controllers.models.ProfileModel;
+import IE.server.services.Student;
+import IE.server.services.UnitSelectionSystem;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -7,8 +10,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "")
 public class HomeController {
 
-    @GetMapping("/{id}")
-    public String getHomeData(@PathVariable String id) {
-        return id;
+    @GetMapping("profiles/{id}")
+    public ProfileModel getHomeData(@PathVariable String id) {
+        try {
+            Student student = UnitSelectionSystem.getInstance().findStudent(id);
+            String name = student.getName();
+            String secondName = student.getSecondName();
+            String stdId = student.getId();
+            String birthDate = student.getBirthDate();
+            String gpa = String.valueOf(student.getReportCard().calcGPA()).substring(0, 5);
+            String tpu = String.valueOf(student.getReportCard().calcTPU());
+            String faculty = student.getFaculty();
+            String field = student.getField();
+            String level = student.getLevel();
+            String status = student.getStatus();
+            String img = student.getImg();
+            return new ProfileModel(stdId, name, secondName, birthDate, field, faculty, level, status, img, gpa, tpu);
+        } catch (Exception ignored) { }
+        return null;
     }
 }
---
+++ src/main/java/IE/server/controllers/models/ProfileModel.java
@@ -0,0 +1,74 @@
+package IE.server.controllers.models;
+
+public class ProfileModel {
+    private String stdId;
+    private String name;
+    private String secondName;
+    private String birthDate;
+    private String field;
+    private String faculty;
+    private String level;
+    private String status;
+    private String img;
+    private String gpa;
+    private String tpu;
+
+    public ProfileModel(String stdId, String name, String secondName, String birthDate, String field,
+                        String faculty, String level, String status, String img, String gpa, String tpu) {
+        this.stdId = stdId;
+        this.name = name;
+        this.secondName = secondName;
+        this.birthDate = birthDate;
+        this.field = field;
+        this.faculty = faculty;
+        this.level = level;
+        this.status = status;
+        this.img = img;
+        this.gpa = gpa;
+        this.tpu = tpu;
+    }
+
+    public String getStdId() {
+        return stdId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public String getGpa() {
+        return gpa;
+    }
+
+    public String getTpu() {
+        return tpu;
+    }
+}
